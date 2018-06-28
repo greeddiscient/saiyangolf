7c570acb8aa912af204c72438252c5a27413d387
Shaun Djuhari <shaundjuhari@berkeley.edu>
2018-06-28 23:04:42 +0700

    bunch of SGs and firs and girs but untested

--- RoundDetails.js
+++ RoundDetails.js
@@ -11,12 +11,74 @@ export default class RoundDetailsScreen extends React.Component {
     super(props);
     this.state = {
       drivingSG: 0,
-      drivingDistance: 0
+      drivingDistance: 0,
+      chippingSG: 0,
+      approachSG: 0,
+      totalPutts: 0,
+      totalSG: 0,
+      totalPuttingSG: 0,
+      wedgeSG: 0,
+      gir: 0,
+      fairways: 0
     };
   }
   componentDidMount(){
     this.calculateDrivingDistance()
     this.calculateDrivingStrokesGained()
+    this.calculateChippingStrokesGained()
+    this.calculateApproachStrokesGained()
+    this.calculateWedgeStrokesGained()
+    this.calculatePuttsCountSGTotalSG()
+    this.calculateGIR()
+    this.calculateFairways()
+  }
+  calculateGIR(){
+    const { navigation } = this.props;
+    const roundSummary = navigation.getParam('roundSummary', [])
+    var gir=0
+    for(var i=0;i<roundSummary.length;i++){
+      if(roundSummary[i].par==3 && roundSummary[i].shots.length==1){
+        gir+=1
+      }
+      else if(roundSummary[i].par==4 && roundSummary[i].shots.length<=2){
+        gir+=1
+      }
+      else if(roundSummary[i].par==5 && roundSummary[i].shots.length<=3){
+        gir+=1
+      }
+    }
+    this.setState({gir: gir})
+  }
+  calculateFairways(){
+    const { navigation } = this.props;
+    const roundSummary = navigation.getParam('roundSummary', [])
+    var fairways=0
+    for(var i=0;i<roundSummary.length;i++){
+      if(roundSummary[i].par==4 && roundSummary[i].shots[1].lie=="F"){
+        fairways+=1
+      }
+      else if(roundSummary[i].par==5 && roundSummary[i].shots[1].lie=="F"){
+        fairways+=1
+      }
+    }
+    this.setState({fairways: fairways})
+  }
+  calculatePuttsCountSGTotalSG(){
+    const { navigation } = this.props;
+    const roundSummary = navigation.getParam('roundSummary', [])
+    var totalPutts=0
+    var totalPuttingSG=0
+    var totalSG=0
+    for(var i=0;i<roundSummary.length;i++){
+      totalPutts+=parseInt(roundSummary[i].putts)
+      totalPuttingSG+=parseFloat(roundSummary[i].puttingSG)
+      totalSG+=parseFloat(roundSummary[i].sg)
+    }
+    this.setState({
+      totalPutts: totalPutts,
+      totalPuttingSG: totalPuttingSG,
+      totalSG: totalSG
+    })
   }
   calculateDrivingStrokesGained(){
     const { navigation } = this.props;
@@ -28,13 +90,67 @@ export default class RoundDetailsScreen extends React.Component {
       }
       else{
         drivingSG =drivingSG + parseFloat(roundSummary[i].shots[0].sg)
-        console.log("shot1sg",roundSummary[i].shots[0].sg)
-        console.log("totaldrivingSG",drivingSG)
       }
     }
 
     this.setState({
       drivingSG: drivingSG
+    })
+  }
+  calculateWedgeStrokesGained(){
+    const { navigation } = this.props;
+    const roundSummary = navigation.getParam('roundSummary', [])
+    var wedgeSG=0
+    for(var i=0;i<roundSummary.length;i++){
+
+      for(var j=0;j<roundSummary[i].shots.length;j++){
+        if(roundSummary[i].shots[j].distance <= 125 &&roundSummary[i].shots[j].distance > 50 && roundSummary[i].shots[j].lie != "S" && roundSummary[i].shots[j].lie !="RC"){
+          wedgeSG+=parseFloat(roundSummary[i].shots[j].sg)
+        }
+      }
+    }
+
+    this.setState({
+      wedgeSG: wedgeSG
+    })
+  }
+  calculateChippingStrokesGained(){
+    const { navigation } = this.props;
+    const roundSummary = navigation.getParam('roundSummary', [])
+    var chippingSG=0
+    for(var i=0;i<roundSummary.length;i++){
+
+      for(var j=0;j<roundSummary[i].shots.length;j++){
+        if(roundSummary[i].shots[j].distance <= 50 && roundSummary[i].shots[j].lie != "S" && roundSummary[i].shots[j].lie !="RC"){
+          chippingSG+=parseFloat(roundSummary[i].shots[j].sg)
+        }
+      }
+    }
+
+    this.setState({
+      chippingSG: chippingSG
+    })
+  }
+  calculateApproachStrokesGained(){
+    const { navigation } = this.props;
+    const roundSummary = navigation.getParam('roundSummary', [])
+    var approachSG=0
+    for(var i=0;i<roundSummary.length;i++){
+      if(roundSummary[i].par==3){
+        approachSG+= parseFloat(roundSummary[i].shots[0].sg)
+      }
+      else{
+        for(var j=1;j<roundSummary[i].shots.length;j++){
+          if(roundSummary[i].shots[j].distance > 50){
+            approachSG+=parseFloat(roundSummary[i].shots[j].sg)
+          }
+        }
+      }
+
+    }
+
+    this.setState({
+      approachSG: approachSG
     })
   }
   calculateDrivingDistance(){
@@ -51,7 +167,6 @@ export default class RoundDetailsScreen extends React.Component {
         totalDrivingDistance += parseInt(roundSummary[i].drivingDistance)
       }
     }
-    console.log("totalDrivingDistance",totalDrivingDistance)
     averageDrivingDistance= totalDrivingDistance/count
     this.setState({
       drivingDistance: averageDrivingDistance
@@ -72,8 +187,15 @@ export default class RoundDetailsScreen extends React.Component {
       rows.push(<Text >PuttingSG={roundSummary[i].puttingSG}</Text>)
       rows.push(<Text >TotalSG={roundSummary[i].sg}</Text>)
     }
-    rows.push(<Text >Driving Distance={this.state.drivingDistance}</Text>)
-    rows.push(<Text >Driving SG={this.state.drivingSG}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Driving Distance= {this.state.drivingDistance}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>No of Putts= {this.state.totalPutts}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>GIR= {this.state.gir}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Fairways= {this.state.fairways}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Driving SG= {this.state.drivingSG}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Approach SG= {this.state.approachSG}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Chipping SG= {this.state.chippingSG}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Putting SG= {this.state.totalPuttingSG}</Text>)
+    rows.push(<Text style={{fontWeight:'bold'}}>Total SG= {this.state.totalSG}</Text>)
     console.log(this.props)
 
     return (
