27e824bc2e46e4bb88d1cbca447b2f72e07a1f27
adawiyah <rofiatul.adawiyah2@gmail.com>
2019-02-04 16:04:56 +0700

    change app icon

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -209,6 +209,7 @@ class HomeScreen extends Component {
 
   async onRefresh() {
     if (!this.state.refreshing) {
+      await this.setState({ rounds: [] })
       this.loadDataFromServer();
     }
   }
