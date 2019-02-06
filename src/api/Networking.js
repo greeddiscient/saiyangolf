e173b0d521cc68dcb9fe0b939fdecde0537e2b2a
adawiyah <rofiatul.adawiyah2@gmail.com>
2019-02-06 17:51:28 +0700

    implement api register

--- src/api/Networking.js
+++ src/api/Networking.js
@@ -31,6 +31,8 @@ async function Login(email, password) {
         }
       }
       else {
+        console.log('error login..., '+JSON.stringify(json))
+        console.log('status login, '+res.info().status)
         setTimeout(() => Alert.alert('error', json.errors.user), 100);
         response = null;
       }
@@ -64,8 +66,118 @@ async function Login(email, password) {
   return response;
 }
 
+async function Register(data) {
+  var response = null;
+  await RNFetchBlob.fetch('POST', baseUrl + '/api/users/register', {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }, JSON.stringify({
+    name: data.name,
+    email: data.email,
+    password: data.password,
+  }))
+    // when response status code is 200
+    .then((res) => {
+      let text = res.text();
+      let json = res.json();
+
+      if (res.info().status < 300) {
+        if (json.success == true) {
+          AsyncStorage.setItem('@saiyanGolfStore:register', json['data']);
+          response = json.userCreated;
+        } else {
+          setTimeout(() => Alert.alert('error', json), 100);
+          response = null;
+        }
+      }
+      else {
+        if (json.success == true) {
+          setTimeout(() => Alert.alert('error', json), 100);
+          response = null;
+        } else {
+          setTimeout(() => Alert.alert('error', JSON.stringify(json)), 100);
+          response = null;
+        }
+      }
+    })
+    // Status code is not 200
+    .catch((errorMessage, statusCode) => {
+      // error handling
+      console.log('errors register' + errorMessage + statusCode);
+      if (errorMessage == null || errorMessage == '') {
+        setTimeout(() => Alert.alert(
+          'Failed',
+          'Something wrong. Please try again',
+          [
+            { text: 'Ok' },
+          ]
+        ), 100);
+        response = null;
+      }
+      else {
+        setTimeout(() => Alert.alert(
+          'Failed',
+          errorMessage.toString(),
+          [
+            { text: 'Ok' },
+          ]
+        ), 100);
+        response = null;
+      }
+    })
+
+  return response;
+}
+
+async function getDataRoundHistory() {
+  var response = null;
+  var token = null;
+
+  await AsyncStorage.getItem('@saiyanGolfStore:tokens', (err, result) => {
+    if (result != null) {
+      token = result;
+    }
+  });
+
+  if (token != null && token != "") {
+    await RNFetchBlob.fetch(
+      'GET',
+      baseUrl + '/api/rounds/',
+      Header.getHeaderToken(token)
+    )
+      .then((res) => {
+        // the following conversions are done in js, it's SYNC
+        let text = res.text()
+        let json = res.json()
+
+        if (res.info().status < 300) {
+          if(json.success == true){
+            data = json['data'];
+            AsyncStorage.setItem('@saiyanGolfStore:roundHistory', JSON.stringify(data));
+            response = data;
+          }else{
+            response = null
+          }
+         
+        }
+        else {
+          response = null
+        }
+      })
+      // Status code is not 200
+      .catch((errorMessage, statusCode) => {
+        // error handling
+        response = null;
+      })
+
+    return response
+  }
+}
+
 export default {
   protocol,
   baseUrl,
+  Register,
   Login,
+  getDataRoundHistory
 }
--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -8,7 +8,9 @@ import {
   StatusBar,
   RefreshControl,
   ScrollView,
+  Platform,
   AsyncStorage,
+  Alert,
 } from 'react-native';
 import {
   Text,
@@ -18,6 +20,10 @@ import {
   Footer,
   StyleProvider,
 } from 'native-base';
+import {
+  NavigationActions,
+  StackActions,
+} from 'react-navigation';
 import * as Progress from 'react-native-progress';
 
 import colors from '../../config/colors';
@@ -26,6 +32,7 @@ import getTheme from '../../../native-base-theme/components';
 import material from '../../../native-base-theme/variables/material';
 import ImageUrl from '../../config/images';
 import Function from '../../utilities/Function';
+import Networking from '../../api/Networking';
 
 const { width, height } = Dimensions.get("window");
 class Register extends Component {
@@ -37,119 +44,166 @@ class Register extends Component {
       email: '',
       password: '',
       confirmPass: '',
+      tokens: null,
+      isLoading: true,
+      visibleLoading: false
     };
+
+    AsyncStorage.getItem(
+      '@saiyanGolfStore:tokens'
+      , (err, result) => {
+        if (result !== null) {
+          this.setState({ tokens: result })
+        }
+
+        this.loginCheck();
+      })
+  }
+
+  loginCheck() {
+    try {
+      if (this.state.tokens == null || this.state.tokens == "") {
+        this.setState({ isLoading: false });
+      } else {
+        this.renderHome()
+      }
+    } catch (e) {
+      setTimeout(() => this.setState({ isLoading: false }), 200);
+    }
   }
 
   render() {
-    return (
-      <StyleProvider style={getTheme(material)}>
-        <Container>
-          <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
-          <Image source={ImageUrl.bgImg} style={styles.bgHome} />
-          <Content>
-            <View>
-              <View style={styles.containerImageRegister}>
-                <Image
-                  resizeMode='contain'
-                  source={ImageUrl.golfersImg}
-                  style={styles.styleImageGolfer}
-                />
-              </View>
-              <View style={{ width: width }}>
-                <View style={{ marginLeft: 20 }}>
-                  <View style={{ marginBottom: 20 }}>
-                    <Text style={styles.textTitleAuth}>Register</Text>
-                  </View>
-                  <View style={styles.boxInputAuth}>
-                    <Input
-                      ref="1"
-                      autoCorrect={false}
-                      autoCapitalize='none'
-                      returnKeyType={"next"}
-                      underlineColorAndroid="transparent"
-                      placeholderTextColor={colors.white}
-                      placeholder='Name'
-                      placeholderTextColor={colors.darkGrey4}
-                      style={styles.textInputAuth}
-                      onChangeText={name => this.setState({ name })}
-                      onSubmitEditing={() => Function.focusNextField(this, "2")}
-                      value={this.state.name}
-                    />
-                  </View>
-                  <View style={styles.boxInputAuth}>
-                    <Input
-                      ref="2"
-                      autoCorrect={false}
-                      autoCapitalize='none'
-                      returnKeyType={"next"}
-                      underlineColorAndroid="transparent"
-                      placeholderTextColor={colors.white}
-                      placeholder="Email"
-                      keyboardType={'email-address'}
-                      placeholderTextColor={colors.darkGrey4}
-                      style={styles.textInputAuth}
-                      onChangeText={email => this.setState({ email })}
-                      onSubmitEditing={() => Function.focusNextField(this, "3")}
-                      value={this.state.email}
-                    />
-                  </View>
-                  <View style={styles.boxInputAuth}>
-                    <Input
-                      ref="3"
-                      autoCorrect={false}
-                      autoCapitalize='none'
-                      returnKeyType={"next"}
-                      underlineColorAndroid="transparent"
-                      placeholderTextColor={colors.white}
-                      style={styles.textInputAuth}
-                      secureTextEntry={true}
-                      placeholder='Password'
-                      placeholderTextColor={colors.darkGrey4}
-                      onChangeText={password => this.setState({ password })}
-                      onSubmitEditing={() => Function.focusNextField(this, "4")}
-                      value={this.state.password}
-                    />
-                  </View>
-                  <View style={styles.boxInputAuth}>
-                    <Input
-                      ref="4"
-                      autoCorrect={false}
-                      autoCapitalize='none'
-                      returnKeyType={"next"}
-                      underlineColorAndroid="transparent"
-                      placeholderTextColor={colors.white}
-                      style={styles.textInputAuth}
-                      secureTextEntry={true}
-                      placeholder='Confirm password'
-                      placeholderTextColor={colors.darkGrey4}
-                      onChangeText={confirmPass => this.setState({ confirmPass })}
-                      onSubmitEditing={() => this.buttonPress()}
-                      value={this.state.confirmPass}
-                    />
-                  </View>
-                  <TouchableOpacity
-                    style={styles.buttonNewRound}
-                    onPress={() => this.buttonPress()}
-                  >
-                    <Text style={styles.buttonNew}>Register</Text>
-                  </TouchableOpacity>
+    const formStyles = [styles.form];
+    if (Platform.OS === 'android') formStyles.push(styles.androidform);
+    if (this.state.isLoading == true) {
+      return (
+        <View style={styles.activityIndicator}>
+          <Progress.Circle
+            style={styles.loading}
+            indeterminate={this.state.isLoading}
+            direction="counter-clockwise"
+            color={colors.white}
+          />
+        </View>
+      )
+    }
+    else {
+      return (
+        <StyleProvider style={getTheme(material)}>
+          <Container>
+            <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
+            <Image source={ImageUrl.bgImg} style={styles.bgHome} />
+            <Content>
+              <View>
+                <View style={styles.containerImageRegister}>
+                  <Image
+                    resizeMode='contain'
+                    source={ImageUrl.golfersImg}
+                    style={styles.styleImageGolfer}
+                  />
                 </View>
-                <View style={styles.footerContent}>
-                  <Text style={styles.footerText}>
-                    Already have an account?
+                <View style={{ width: width }}>
+                  <View style={{ marginLeft: 20 }}>
+                    <View style={{ marginBottom: 20 }}>
+                      <Text style={styles.textTitleAuth}>Register</Text>
+                    </View>
+                    <View style={styles.boxInputAuth}>
+                      <Input
+                        ref="1"
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        returnKeyType={"next"}
+                        underlineColorAndroid="transparent"
+                        placeholderTextColor={colors.white}
+                        placeholder='Name'
+                        placeholderTextColor={colors.darkGrey4}
+                        style={styles.textInputAuth}
+                        onChangeText={name => this.setState({ name })}
+                        onSubmitEditing={() => Function.focusNextField(this, "2")}
+                        value={this.state.name}
+                      />
+                    </View>
+                    <View style={styles.boxInputAuth}>
+                      <Input
+                        ref="2"
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        returnKeyType={"next"}
+                        underlineColorAndroid="transparent"
+                        placeholderTextColor={colors.white}
+                        placeholder="Email"
+                        keyboardType={'email-address'}
+                        placeholderTextColor={colors.darkGrey4}
+                        style={styles.textInputAuth}
+                        onChangeText={email => this.setState({ email })}
+                        onSubmitEditing={() => Function.focusNextField(this, "3")}
+                        value={this.state.email}
+                      />
+                    </View>
+                    <View style={styles.boxInputAuth}>
+                      <Input
+                        ref="3"
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        returnKeyType={"next"}
+                        underlineColorAndroid="transparent"
+                        placeholderTextColor={colors.white}
+                        style={styles.textInputAuth}
+                        secureTextEntry={true}
+                        placeholder='Password'
+                        placeholderTextColor={colors.darkGrey4}
+                        onChangeText={password => this.setState({ password })}
+                        onSubmitEditing={() => Function.focusNextField(this, "4")}
+                        value={this.state.password}
+                      />
+                    </View>
+                    <View style={styles.boxInputAuth}>
+                      <Input
+                        ref="4"
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        returnKeyType={"next"}
+                        underlineColorAndroid="transparent"
+                        placeholderTextColor={colors.white}
+                        style={styles.textInputAuth}
+                        secureTextEntry={true}
+                        placeholder='Confirm password'
+                        placeholderTextColor={colors.darkGrey4}
+                        onChangeText={confirmPass => this.setState({ confirmPass })}
+                        onSubmitEditing={() => this.onRegisterPress()}
+                        value={this.state.confirmPass}
+                      />
+                    </View>
+                    <TouchableOpacity
+                      style={styles.buttonNewRound}
+                      onPress={() => this.onRegisterPress()}
+                    >
+                      <Text style={styles.buttonNew}>Register</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.footerContent}>
+                    <Text style={styles.footerText}>
+                      Already have an account?
                   </Text>
-                  <TouchableOpacity
-                    onPress={() => this.onLoginPress()}
-                  >
-                    <Text style={styles.footerTextTwo}> Login here</Text>
-                  </TouchableOpacity>
+                    <TouchableOpacity
+                      onPress={() => this.onLoginPress()}
+                    >
+                      <Text style={styles.footerTextTwo}> Login here</Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-            </View>
-          </Content>
-        </Container>
-      </StyleProvider>
-    );
+            </Content>
+            {this.state.visibleLoading && <Progress.Circle
+              style={styles.loading}
+              indeterminate={this.state.visibleLoading}
+              direction="counter-clockwise"
+            />
+            }
+          </Container>
+        </StyleProvider>
+      );
+    }
   }
 
   onLoginPress() {
@@ -158,11 +212,114 @@ class Register extends Component {
     )
   }
 
-  buttonPress() {
-    Function.Home(
-      this.props.navigation
-    )
+  // buttonPress() {
+  //   Function.Home(
+  //     this.props.navigation
+  //   )
+  // }
+
+  valueChecker() {
+    if (this.state.name != '' &&
+      this.state.email != '' &&
+      this.state.password != '' &&
+      this.state.confirmPass != '' &&
+      this.state.password == this.state.confirmPass) {
+      return true
+    }
+    else {
+      if (this.state.name == '') {
+        Alert.alert('Error', 'Please fill name field', [{ text: 'Ok' }])
+        return false
+      }
+      else if (this.state.email == '') {
+        Alert.alert('Error', 'Please fill email field', [{ text: 'Ok' }])
+        return false
+      }
+      else if (this.state.password == '') {
+        Alert.alert('Error', 'Please fill password field', [{ text: 'Ok' }])
+        return false
+      }
+      else if (this.state.confirmPass == '') {
+        Alert.alert('Error', 'Please fill confirm password field', [{ text: 'Ok' }])
+        return false
+      }
+      else if (this.state.password != this.state.confirmPass) {
+        Alert.alert('Error', "The new password confirmation does not match.", [{ text: 'Ok' }])
+        return false;
+      } else {
+        Alert.alert('Error', 'Please fill empty value', [{ text: 'Ok' }])
+        return false;
+      }
+    }
   }
+
+  async onRegisterPress(target) {
+    if (this.valueChecker() == true && this.state.visibleLoading == false) {
+      this.setState({ visibleLoading: true })
+      var dataPost = {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+      }
+
+      console.log('register data, '+JSON.stringify(dataPost))
+
+      const RegisterProcess = await Networking.Register(dataPost)
+      if (RegisterProcess != null) {
+        this.setState({
+          visibleLoading: false,
+        })
+
+        Alert.alert("Success", "Member is successfully registered.", [
+          {
+            text: "Ok", onPress: () => {
+              this.props.navigation.goBack()
+            }
+          },
+        ]);
+        // this.renderHome()
+        
+      } else {
+        this.setState({ visibleLoading: false })
+        this.ableToEdit();
+      }
+    }
+  }
+
+  ableToEdit = () => {
+    this.setState({ visibleLoading: false })
+    if (this.refs["1"] != null) {
+      this.refs["1"].setNativeProps({ editable: true })
+    }
+    if (this.refs["2"] != null) {
+      this.refs["2"].setNativeProps({ editable: true })
+    }
+  }
+
+  renderHome() {
+    const resetAction = StackActions.reset({
+      index: 0,
+      key: null,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Home' })
+      ]
+    })
+
+    this.props.navigation.dispatch(resetAction)
+  }
+
+  renderLogin() {
+    const resetAction = StackActions.reset({
+      index: 0,
+      key: null,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Login' })
+      ]
+    })
+
+    this.props.navigation.dispatch(resetAction)
+  }
+
 
 }
 
